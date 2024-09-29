ebc9b7e5ddc103c62346b20fcf303b419c34c16d
Peter Strössler <7starch@gmail.com>
2024-09-29 15:41:18 +0200

    fresh commit

--- install.py
+++ install.py
@@ -1,8 +1,3 @@
-from flask import Flask, render_template, session, redirect, url_for, session
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField
-from wtforms.widgets import TextArea
-from wtforms import SubmitField, StringField
 from langchain_community.llms.mlx_pipeline import MLXPipeline
 
 pipe = MLXPipeline.from_model_id(
